496a6bb066bda68a49a75b6345f2a999aa92cdcd
lowsk <LowSk1ll.aa@gmail.com>
2024-01-26 15:01:38 +0200

    d

---
+++ src/main/java/com/javarush/hibernate/malacion/domain/Continent.java
@@ -0,0 +1,11 @@
+package com.javarush.hibernate.malacion.domain;
+
+public enum Continent {
+    ASIA,
+    EUROPE,
+    NORTH_AMERICA,
+    AFRICA,
+    OCEANIA,
+    ANTARCTICA,
+    SOUTH_AMERICA
+}
